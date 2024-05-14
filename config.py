21aee5f771ff7f575f92935f29301ab3cdf12a61
Spencer Smith <57018006+SpenceHer@users.noreply.github.com>
2024-05-14 10:36:42 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,18 @@
 from google.oauth2 import service_account
 from google.cloud import vision
+import os
 
-def get_vision_client(credentials_path):
-    """ Sets up and returns a Google Vision API client. """
-    credentials = service_account.Credentials.from_service_account_file(
-        credentials_path)
+def get_vision_client():
+    # Get the path to the credentials JSON file from the environment variable
+    credentials_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    if not credentials_path:
+        raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
+    
+    # Use the environment variable to load credentials
+    credentials = service_account.Credentials.from_service_account_file(credentials_path)
     client = vision.ImageAnnotatorClient(credentials=credentials)
     return client
+
+
 
 
